f75adb01a1ae71e4ab55569b31577e79965ed421
Aggrathon <antonbjo@gmail.com>
2017-12-02 12:41:27 +0200

    UI Mockup

---
+++ Assets/Scripts/Equipment.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Equipment")]
+public class Equipment : ScriptableObject {
+
+    public enum Slots
+    {
+        head,
+        body,
+        rightHand,
+        leftHand,
+        bothHands
+    }
+
+    public enum Action
+    {
+        none,
+        damage,
+        damageStrength,
+        damageAgility,
+        block,
+        move
+    }
+
+    public Sprite icon;
+    public Slots slot;
+    public Ability[] actions;
+    public int durability = 20;
+    [Space]
+    public int strength;
+    public int agility;
+    public int constitution;
+    public int intelligence;
+
+    [System.Serializable]
+    public struct Ability
+    {
+        public int cooldown;
+        public string name;
+        public Action action;
+        public float amount;
+    }
+}
